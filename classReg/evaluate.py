2c15bded88d022b5805c80ce35749a2b6df4ae8f
Yordanos Abraham Goshu <yag3@se3-compute07.cs.cornell.edu>
2020-01-11 19:40:07 -0500

    accepting multiple files

--- classReg/evaluate.py
+++ classReg/evaluate.py
@@ -1,36 +1,47 @@
 import imageio
 import numpy as np
 from keras.models import model_from_json
-
-#load json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
-# load weights into new model
-loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
-
-
 from PIL import Image
 from resizeimage import resizeimage
 
-fd_img = open('../fewshot4/generate_5_8857_5_38855.png', 'rb')
-#fd_img = imageio.imread('generate_10829.png')
-img = Image.open(fd_img)
-img.show()
-#print(np.asarray(img))
-resized_img = np.asarray(resizeimage.resize_contain(img, [28, 28]))
-resized_img = resized_img[ :, :, 0]
-#img.save('generate_10829_28.png', img.format)
-fd_img.close()
+def load_classifier(model_json, model_h5):
+    #load json and create model
+    json_file = open(model_json, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(model_h5)
+    print("Loaded model from disk")
+    return loaded_model
 
-#test_img = imageio.imread('generate_10829_28.png')
+def get_class_prob_form_file(loaded_model, files):
+    for file_n in files:
+        print('reading image')
+        fd_img = imageio.imread(file_n)
+        img = Image.fromarray(fd_img)
+        resized_img = np.asarray(resizeimage.resize_thumbnail(img, [28, 28]))
+        print('image resized')
 
-print('original size\n')
-#print(fd_img.shape)
-print('resized\n')
-print(resized_img.shape)
-#print(resized_img)
-pred = loaded_model.predict(resized_img.reshape(1, 28, 28, 1))
-print(pred)
+        resized_img = resized_img.astype('float32') 
+        resized_img = resized_img/255
+        pred = loaded_model.predict(resized_img.reshape(1,28, 28, 1)) 
+        print(file_n)
+        print(pred.argmax())
+        print(pred[0][pred[0].argmax()])
+
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model_json', type=str)
+    parser.add_argument('--model_h5', type=str)
+    parser.add_argument('--files', nargs='+', type=str)
+    config = parser.parse_args()
+    print('loaded_model')
+    loaded_model = load_classifier(config.model_json, config.model_h5)
+    print('getting probs')
+    get_class_prob_form_file(loaded_model, config.files)
+if __name__== "__main__" :
+    main()
